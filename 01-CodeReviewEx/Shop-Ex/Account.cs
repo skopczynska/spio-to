3b373fd170faa4e6b8aedf0615edab9107f4baf6
Sylwia <sylwia.kopczynska@cs.put.poznan.pl>
2022-02-27 12:35:42 +0100

    Adds calculaiton of fee

---
+++ 01-CodeReviewEx/Shop-Ex/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Ex
+{
+    public class Account
+    {
+        public double principal, rate; int daysActive, accountType;
+        public static const int STANDARD = 0, BUDGET = 1, PREMIUM = 2, PREMIUM_PLUS = 3;
+    }
+}
